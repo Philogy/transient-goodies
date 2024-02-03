b78c3f62a1aba37d20f5a503b85913fe4fc329ba
Philippe Dumonet <philippe@dumo.net>
2024-02-03 15:41:46 +0100

    ♻️ Rename `data` to `tbytes` in test

--- test/TransientBytes.t.sol
+++ test/TransientBytes.t.sol
@@ -6,52 +6,53 @@ import {TransientBytes} from "../src/TransientBytesLib.sol";
 
 /// @author philogy <https://github.com/philogy>
 contract TransientBytesTest is Test {
-    TransientBytes data;
+    TransientBytes tbytes;
 
     uint256 internal constant MAX_LENGTH = type(uint32).max;
 
     function test_defaultEmpty() public {
-        assertEq(data.get(), "");
-        assertEq(data.length(), 0);
+        assertEq(tbytes.get(), "");
+        assertEq(tbytes.length(), 0);
     }
 
     function test_setMem(bytes memory inner) public {
         vm.assume(inner.length <= MAX_LENGTH);
-        data.set(inner);
-        assertEq(data.get(), inner);
-        assertEq(data.length(), inner.length);
-        data.agus();
-        assertEq(data.get(), "");
-        assertEq(data.length(), 0);
+        tbytes.set(inner);
+        assertEq(tbytes.get(), inner);
+        assertEq(tbytes.length(), inner.length);
+        tbytes.agus();
+        assertEq(tbytes.get(), "");
+        assertEq(tbytes.length(), 0);
     }
 
     function test_setCd(bytes calldata inner) public {
         vm.assume(inner.length <= MAX_LENGTH);
-        data.setCd(inner);
-        assertEq(data.get(), inner);
-        assertEq(data.length(), inner.length);
-        data.agus();
-        assertEq(data.get(), "");
-        assertEq(data.length(), 0);
+        tbytes.setCd(inner);
+        assertEq(tbytes.get(), inner);
+        assertEq(tbytes.length(), inner.length);
+        tbytes.agus();
+        assertEq(tbytes.get(), "");
+        assertEq(tbytes.length(), 0);
     }
 
     function test_multipleSetMemNoAgus(bytes memory inner1, bytes memory inner2) public {
         vm.assume(inner1.length <= MAX_LENGTH);
         vm.assume(inner2.length <= MAX_LENGTH);
 
-        data.set(inner1);
-        assertEq(data.get(), inner1);
-        data.set(inner2);
-        assertEq(data.get(), inner2);
+        tbytes.set(inner1);
+        assertEq(tbytes.get(), inner1);
+        tbytes.set(inner2);
+        assertEq(tbytes.get(), inner2);
     }
 
     function test_multipleSetCdNoAgus(bytes calldata inner1, bytes calldata inner2) public {
         vm.assume(inner1.length <= MAX_LENGTH);
         vm.assume(inner2.length <= MAX_LENGTH);
 
-        data.setCd(inner1);
-        assertEq(data.get(), inner1);
-        data.setCd(inner2);
-        assertEq(data.get(), inner2);
+        tbytes.setCd(inner1);
+        assertEq(tbytes.get(), inner1);
+        tbytes.setCd(inner2);
+        assertEq(tbytes.get(), inner2);
+    }
     }
 }
