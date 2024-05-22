0265913bd7f8eb352c85d7057904c43ee641d5e3
Cal Bera <calbera@berachain.com>
2024-05-22 14:23:56 -0400

    revert

--- test/PayableMulticallable.t.sol
+++ test/PayableMulticallable.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {console,Test} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {stdError} from "forge-std/StdError.sol";
 import {Multicallable} from "./mocks/Multicallable.sol";
 
